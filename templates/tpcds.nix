10fdf3dc4212a592af65a15e4fe3c896a540adab
unknown <crystyxn@yahoo.com>
2019-06-15 22:11:09 +0200

    dsqgen works now
    Co-authored-by: Peter Ferenczy <ggpeti@gmail.com>

--- templates/tpcds.nix
+++ templates/tpcds.nix
@@ -9,13 +9,18 @@ pkgs.stdenv.mkDerivation rec {
     sha256 = "0l1jn2k4n9cyvf3i4bjkirqpz77d42jv13yzwg34rwlzckrvybx5";
   };
   preBuild = "cd tools";
-  buildInputs = with pkgs; [ bison byacc (pkgs.writeScriptBin "lex" "exec ${flex}/bin/flex $@") ];
+  buildInputs = with pkgs; [ makeWrapper bison byacc (pkgs.writeScriptBin "lex" "exec ${flex}/bin/flex $@") ];
   hardeningDisable = [ "all" ];
   
   installPhase = ''
     mkdir -p $out/bin
-    cp dsdgen dsqgen distcomp mkheader checksum $out/bin;
-        
+    cp -r dsdgen dsqgen distcomp mkheader checksum .ctags_updated $out/bin
+    mkdir -p $out/share
+    cp -r tpcds.idx ../query_templates $out/share
+    
   '';
-  
+
+  postFixup = ''wrapProgram $out/bin/dsdgen --run "cd $out/share"
+                wrapProgram $out/bin/dsqgen --add-flags "-directory query_templates" --run "cd $out/share"'';
 }
+
