41fcc9b6db114d5d91b1a003f7f226d9b6b4dee8
Márton Boros <martonboros@gmail.com>
2019-08-18 11:50:47 +0000

    cleanup

--- nixos/packages/hadoop.nix
+++ nixos/packages/hadoop.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> {}, worker_ips? ["localhost"], ... }:
 pkgs.hadoop_3_1.overrideAttrs (oldAttrs: {
-  installPhase = builtins.replaceStrings ["HADOOP_PREFIX"] ["HADOOP_HOME"] oldAttrs.installPhase
-  + ''
-    echo "${pkgs.lib.concatStringsSep "\n" worker_ips}" > $out/etc/hadoop/workers
+  installPhase = builtins.replaceStrings ["HADOOP_PREFIX"] ["HADOOP_HOME"] oldAttrs.installPhase + ''
+    echo "${pkgs.lib.concatStringsSep "\n" worker_ips}\n" > $out/etc/hadoop/workers
   '' ;
 })
